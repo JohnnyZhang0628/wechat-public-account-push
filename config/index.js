2220da506a6e2d996779ecf8d07685e6a6138596
johnny <237183141@qq.com>
2022-09-08 14:27:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,21 @@ export const config = {
       // 所在城市
       city: "上海",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://sports.cctv.com/snooker/"
+    }, 
+     {
+      // 想要发送的人的名字
+      name: "佳昕",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ogedc5oiZF7rSAK29gwBBnRO69fY",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "3HYM5t0htMSbqftRwPrauU8hPct7CsW3jLblhlz5Jeo",
+      // 所在省份
+      province: "上海",
+      // 所在城市
+      city: "上海",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://sports.cctv.com/snooker/"
     }, 
    
   ],
@@ -83,8 +97,7 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "佳昕", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "练习台球", "year": "2022", "date": "09-08"},
+    {"type": "生日", "name": "佳昕", "year": "1998", "date": "09-01"},
   ],
 
   /**
@@ -102,11 +115,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
-    // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
-    // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    {"keyword": "train", date: "2022-09-07"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
